d31522e36c722de83d0bb01ed06a953a67b6c458
ihsajjad <ihsajjad1@gmail.com>
2024-01-22 22:02:42 +0600

    customized tailwind config file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,9 @@ export default function Home() {
   return (
     <MaxWidthWrapper className="mb-12 mt-28 sm:mt-40 flex items-center justify-center flex-col text-center">
       <div className="mx-auto mb-4 flex max-w-fit items-center justify-center space-x-2 overflow-hidden rounded-full border border-gray-200 bg-white px-7 py-2 shadow-md backdrop-blur transition-all hover:border-gray-300 hover:bg-white/50">
-        Hello
+        <p className="text-sm font-semibold text-gray-700">
+          Quill is now public!
+        </p>
       </div>
     </MaxWidthWrapper>
   );
