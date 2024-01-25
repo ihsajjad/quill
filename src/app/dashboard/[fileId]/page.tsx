ddfbda01cde065e31d23be8048f3eae1fcf36d70
Iftekher Hossen Sajjad <iftekherhossensajjad@gmail.com>
2024-01-25 11:45:02 +0600

    added PDFRenderer and ChatWrapper file

--- src/app/dashboard/[fileId]/page.tsx
+++ src/app/dashboard/[fileId]/page.tsx
@@ -1,3 +1,5 @@
+import ChatWrapper from "@/components/ChatWrapper";
+import PDFRenderer from "@/components/PDFRenderer";
 import { db } from "@/db";
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { notFound, redirect } from "next/navigation";
@@ -25,11 +27,18 @@ const Page = async ({ params }: PageProps) => {
   if (!file) notFound();
 
   return (
-    <div className="flex-1 flex flex-col justify-between h-[calc(100vh - 3.5rem)] ">
+    <div className="flex-1 flex flex-col justify-between h-[calc(100vh-3.5rem)] ">
       <div className="mx-auto w-full max-w-8xl grow lg:flex xl:px-2">
         {/* left side */}
-        <div className="flex-1 xl:flex-">
-          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6"></div>
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            <PDFRenderer />
+          </div>
+        </div>
+
+        {/* right side */}
+        <div className="shrink-0 flex-[0.75] border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
         </div>
       </div>
     </div>
