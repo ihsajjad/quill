607c51f0b08131abe9580ede8b56f49e12dc1ae6
ihsajjad <ihsajjad1@gmail.com>
2024-02-01 22:16:57 +0600

    added stripe webhook

--- src/app/api/auth/[kindeAuth]/route.ts
+++ src/app/api/auth/[kindeAuth]/route.ts
@@ -1,7 +1,7 @@
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 import { NextRequest } from "next/server";
-
-export const GET = (request: NextRequest, { params }: any) => {
+import { z } from "zod";
+export const GET = (request: NextRequest, { params }: { params: any }) => {
   const endpoint = params.kindeAuth;
   return handleAuth(request, endpoint);
 };
